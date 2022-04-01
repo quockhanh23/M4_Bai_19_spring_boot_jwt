22d75672e4fb2c9b26b4634883a55b0f12e8c5a2
unknown <yeuthuongvoivangdiqua@gmail.com>
2022-04-01 18:16:46 +0700

    commit

---
+++ src/main/java/com/example/bai19/Bai19Application.java
@@ -0,0 +1,13 @@
+package com.example.bai19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bai19Application.class, args);
+	}
+
+}
